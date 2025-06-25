9acf2085b92d7b0ecbe1aee4d0e31b51599df9fe
Arie Oldman <arie.oldman@vhs7.tv>
2025-06-25 23:46:56 +1000

    styling and validation

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -9,6 +9,23 @@ const gomments = {
       });
   },
   nextIdempotencyKey: null,
+  styleColors: `
+  :host {
+    --accent-color: #ff4757;
+    --accent-color-light: #ff6b81;
+    --body-text-color: #2f3542;
+    --body-text-color-muted: #747d8c;
+    --body-background: #f1f2f6;
+    --body-background-alt: #dfe4ea;
+
+    /* Dark */
+    --body-text-color-dark: #f1f2f6;
+    --body-text-color-muted-dark: #ced6e0;
+    --body-background-dark: #121212;
+    --body-background-dark-alt: #2f3542;
+    --body-background-dark-alt-1: #57606f;
+  }
+  `,
 }
 
 window.gomments = gomments;
@@ -46,6 +63,8 @@ class ReplySubmissionFormComponent extends ReactiveRenderingHTMLElement {
   async render() {
     this.shadowRoot.innerHTML = `
     <style>
+        ${gomments.styleColors}
+
         .row {
             display: flex;
             gap: 10px;
@@ -144,17 +163,17 @@ class ReplySubmissionFormComponent extends ReactiveRenderingHTMLElement {
     <form id="gomments-reply-form" method="post">
         <div class="row">
             <div class="field-group">
-                <label class="field-label" for="name">Name</label>
-                <input type="text" id="name" name="name" placeholder="(optional, max 40 chars)">
+                <label id="gomments-input-name-label" class="field-label" for="name"></label>
+                <input type="text" id="gomments-input-name" name="name" placeholder="(optional, max 40 chars)">
             </div>
             <div class="field-group">
-                <label class="field-label" for="secret">Secret</label>
-                <input type="password" id="secret" name="secret" placeholder="(optional, 10 - 40 chars, for ID)">
+                <label id="gomments-input-secret-label" class="field-label" for="secret"></label>
+                <input type="password" id="gomments-input-secret" name="secret" placeholder="(optional, 10 - 40 chars, for ID)">
             </div>
         </div>
         <div class="row">
             <div class="field-group">
-                <label class="field-label" for="gomments-reply-form-body">Message</label>
+                <label id="gomments-input-body-label" class="field-label" for="gomments-reply-form-body"></label>
                 <textarea id="gomments-reply-form-body" name="body" placeholder="(max 500 characters)"></textarea>
             </div>
         </div>
@@ -165,18 +184,49 @@ class ReplySubmissionFormComponent extends ReactiveRenderingHTMLElement {
     `;
 
     const textarea = this.shadowRoot.querySelector('#gomments-reply-form-body');
+    const inputName = this.shadowRoot.querySelector('#gomments-input-name');
+    const inputSecret = this.shadowRoot.querySelector('#gomments-input-secret');
+    const labelName = this.shadowRoot.querySelector('#gomments-input-name-label');
+    const labelSecret = this.shadowRoot.querySelector('#gomments-input-secret-label');
+    const labelBody = this.shadowRoot.querySelector('#gomments-input-body-label');
     const submitButton = this.shadowRoot.querySelector('.submit-btn');
 
     // Function to check if textarea has content and update button state
     const updateSubmitButtonState = () => {
-        const hasContent = textarea.value.trim().length > 0;
-        submitButton.disabled = !hasContent;
+      const l = textarea.value.trim().length;
+      submitButton.disabled =
+        l <= 0 ||
+        l > 500 ||
+        inputName.value.length > 40 ||
+        ((inputSecret.value.length > 40 || inputSecret.value.length < 10) && inputSecret.value.length != 0);
     };
 
-    // Initial check
+    const setLabelName = () => {
+      const s = inputName.value.length > 40 ? "max 40 chars" : "";
+      labelName.innerHTML = `Name <span style="color: crimson">${s}</span>`;
+    }
+
+    const setLabelSecret = () => {
+      const s = (inputSecret.value.length > 40 || inputSecret.value.length < 10) && inputSecret.value.length != 0 ? "10 – 40 chars" : "";
+      labelSecret.innerHTML = `Secret <span style="color: crimson">${s}</span>`;
+    }
+
+    const setLabelBody = () => {
+      const s = textarea.value.length > 500 ? "max 500 chars" : "";
+      labelBody.innerHTML = `Message <span style="color: crimson">${s}</span>`;
+    }
+
+    setLabelName();
+    setLabelSecret();
+    setLabelBody();
     updateSubmitButtonState();
 
-    // Listen for input changes in the textarea
+    textarea.addEventListener('input', updateSubmitButtonState);
+    inputName.addEventListener('input', setLabelName);
+    inputSecret.addEventListener('input', setLabelSecret);
+    textarea.addEventListener('input', setLabelBody);
+    inputName.addEventListener('input', updateSubmitButtonState);
+    inputSecret.addEventListener('input', updateSubmitButtonState);
     textarea.addEventListener('input', updateSubmitButtonState);
 
     const resetTextArea = () => {
@@ -234,6 +284,8 @@ class ReplyComponent extends ReactiveRenderingHTMLElement {
     const signature = this.getAttribute("reply-signature") || "";
     this.shadowRoot.innerHTML = `
     <style>
+      ${gomments.styleColors}
+
       :host {
         font-family: inconsolata, monospace;
         line-height: 110%;
@@ -268,23 +320,56 @@ class ReplyComponent extends ReactiveRenderingHTMLElement {
         background-color: #ffffff;
       }
 
+      .text-muted {
+        color: var(--body-text-color-muted);
+      }
+
+      .text-normal {
+        color: var(--body-text-color);
+      }
+
       @media (prefers-color-scheme: dark) {
         .has-background {
-          background-color: #2f3542;
+          background-color: var(--body-background-dark-alt);
+        }
+        .text-muted {
+          color: var(--body-text-color-muted-dark);
+        }
+
+        .thick-border-top {
+          border-top: solid 1px var(--body-text-color-muted-dark);
         }
       }
+
+      .heading-font {
+        font-family: 'Manrope';
+      }
+
+      .small-font {
+        font-size: 1.4rem;
+      }
+
+      .body-font {
+        font-family: quattrocento;
+      }
+
+      .thick-border-top {
+        border-top: solid 1px var(--body-text-color-muted);
+        padding-top: 1.4rem;
+      }
     </style>
-      <div class="has-padding has-margin-bottom-m rounded has-background">
-        <div class="italic has-margin-bottom-m">
-          <span>[#${this.getAttribute("reply-id")}] </span>
-          <span class="has-font-weight-bold">${this.getAttribute("reply-author-name")}</span>
-          <span>${signature !== "" ? "(" + signature.slice(-8) + ")" : ""}</span>
-          <span>said at ${createdAt.toLocaleString()}</span>
-        </div>
-        <div>
-          ${this.getAttribute("reply-body")}
-        </div>
+    <div class="has-padding small-font has-margin-bottom-m rounded has-background">
+      <div class="has-margin-bottom-m">
+        <span class="heading-font has-font-weight-bold">${this.getAttribute("reply-author-name")}</span>
+        <span class="heading-font">${signature !== "" ? "(" + signature.slice(-8) + ")" : ""}</span>
       </div>
+      <div class="has-margin-bottom-m body-font">
+        ${this.getAttribute("reply-body")}
+      </div>
+      <div class="italic text-muted body-font thick-border-top">
+        <span>#${this.getAttribute("reply-id")} ${createdAt.toLocaleString()}</span>
+      </div>
+    </div>
     `;
   }
 }
@@ -318,7 +403,7 @@ async function reloadThread() {
   const thread = document.getElementById("comments-thread");
   thread.innerHTML = "<gomments-loading-fill />";
 
-  await new Promise(resolve => setTimeout(() => {resolve();}, 1000));
+  await new Promise(resolve => setTimeout(() => {resolve();}, 500));
 
   return await fetch(`${gomments.baseURL}/articles/${gomments.article}/replies`)
     .then(r => r.json())
