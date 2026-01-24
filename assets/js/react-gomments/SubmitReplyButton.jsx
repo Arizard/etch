fcedadab3161390798fdae125830879454a0cf42
Arie Oldman <arie.oldman@vhs7.tv>
2026-01-24 12:32:53 +1100

    umami in gomments

--- assets/js/react-gomments/SubmitReplyButton.jsx
+++ assets/js/react-gomments/SubmitReplyButton.jsx
@@ -1,6 +1,6 @@
 export default function SubmitReplyButton({ disabled }) {
   return (
-    <button className="submit-btn" type="submit" disabled={disabled}>
+    <button className="submit-btn" type="submit" disabled={disabled} data-umami-event="Submit Reply" data-umami-event-button-disabled={disabled}>
       Submit Reply
     </button>
   );
