a9cfc6329b2425b74f1c895dfe572a77a99befcf
Arie Oldman <arie.oldman@vhs7.tv>
2026-02-27 20:44:52 +1100

    adds alerts (callouts)

--- assets/js/react-gomments/ReplyFeed.jsx
+++ assets/js/react-gomments/ReplyFeed.jsx
@@ -12,7 +12,7 @@ export default function ReplyFeed({
   }
 
   if (error) {
-    return <div className="error-fill">could not load comments</div>;
+    return <div className="error-fill">could not load comments...</div>;
   }
 
   return (
