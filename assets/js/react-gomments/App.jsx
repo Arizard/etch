528690a653d3a96f23f1fef3652013398d1bafa0
Arie Oldman <arie.oldman@vhs7.tv>
2026-04-18 21:21:32 +1000

    add reactions to gomments

--- assets/js/react-gomments/App.jsx
+++ assets/js/react-gomments/App.jsx
@@ -5,6 +5,7 @@ import ReplyFeed from './ReplyFeed';
 
 const baseURL = `${window.siteConfig.apiURL}/gomments`;
 const article = btoa(window.articleConfig.articleID);
+const likeDeletionKeyName = `article:${article}:likeDeletionKey`
 
 export default function App() {
   const [replies, setReplies] = useState([]);
@@ -14,20 +15,68 @@ export default function App() {
   const [attentionReplyId, setAttentionReplyId] = useState('');
   const [idempotencyKey, setIdempotencyKey] = useState(() => uuid4());
 
-  const loadReplies = useCallback(async () => {
+  const [reactionBarLocked, setReactionBarLocked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+  const [liked, setLiked] = useState(false);
+  const handleLikeButtonOnClick = async () => {
+    if (reactionBarLocked) {
+      return;
+    }
+    setReactionBarLocked(true);
+    if (liked) {
+      setLiked(false);
+      const deletionKey = localStorage.getItem(likeDeletionKeyName);
+      if (deletionKey) {
+        await fetch(`${baseURL}/reactions?key=${deletionKey}`, {
+          method: 'DELETE',
+        });
+        localStorage.removeItem(likeDeletionKeyName);
+      }
+    } else {
+      setLiked(true);
+      const resp = await fetch(`${baseURL}/articles/${article}/reactions/like`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (resp.ok) {
+        const { deletion_key: deletionKey } = await resp.json();
+        localStorage.setItem(likeDeletionKeyName, deletionKey);
+      }
+    }
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(), 1000);
+    });
+    setReactionBarLocked(false);
+  }
+
+  const load = useCallback(async () => {
     setError(false);
 
     try {
-      const [statsRes, repliesRes] = await Promise.all([
+      // need to refactor the backend sometime and expose a single endpoint for an article.
+      const [statsRes, repliesRes, reactionsRes] = await Promise.all([
         fetch(`${baseURL}/articles/replies/stats?article=${article}`),
         fetch(`${baseURL}/articles/${article}/replies`),
+        fetch(`${baseURL}/articles/reactions/stats?article=${article}`),
       ]);
 
       const statsData = await statsRes.json();
       const repliesData = await repliesRes.json();
+      const reactionsData = await reactionsRes.json();
 
       setReplyCount(statsData.stats[article].count);
       setReplies(repliesData.replies);
+
+      const likeDeletionKey = localStorage.getItem(likeDeletionKeyName);
+      if (likeDeletionKey) {
+        setLiked(true);
+      }
+
+      setLikeCount(reactionsData.stats[article].like - (likeDeletionKey ? 1 : 0));
+
       setLoading(false);
     } catch (e) {
       console.error('Failed to load comments:', e);
@@ -37,8 +86,8 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    loadReplies();
-  }, [loadReplies]);
+    load();
+  }, [load]);
 
   const handleSubmitReply = async ({ name, secret, body }) => {
     try {
@@ -59,7 +108,7 @@ export default function App() {
         setIdempotencyKey(uuid4());
         const r = await response.json();
         setAttentionReplyId(`${r.reply.id}`);
-        await loadReplies();
+        await load();
         return true;
       } else {
         console.error('Error:', response.status);
@@ -73,6 +122,26 @@ export default function App() {
 
   return (
     <div className="gomments-container">
+    <!-- this may work better as a component -->
+    <div className={reactionBarLocked ? "reaction-bar locked" : "reaction-bar"}>
+    <button
+      className="like-button"
+      aria-pressed={liked}
+      aria-label={liked ? "unlike article" : "like article"}
+      onClick={handleLikeButtonOnClick}
+    >
+      <div>❤️</div>
+    {
+      liked
+        ? likeCount > 0
+          ? <div>Liked by <strong>you</strong> and {likeCount} readers.</div>
+          : <div>Liked by <strong>you</strong>.</div>
+        : likeCount > 0
+          ? <div>Liked by {likeCount} readers.</div>
+          :  <div>Be the first to leave a like.</div>
+    }
+    </button>
+    </div>
     <div className="section-label">Reply</div>
       <ReplyForm onSubmit={handleSubmitReply} />
       <ReplyFeed
